78315d71835b29fec5907bdd26fc79c2a201835d
Josue Del Rio <sandra@MacBook-Pro-de-Sandra-2.local>
2018-03-30 17:26:37 -0600

    commited fix

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,9 @@
-$(window).scroll(function() {
-    var position = $(this).scrollTop();
-    
-    $('.section').each(function() {
-        var target = $(this).offset().top;
-        var id = $(this).attr('id');
-
-        if (position >= target) {
-            $(' #mainNav > ul > li > a').removeClass('active');
-            $('#mainNav > ul > li > a[href="# + id + "]').addClass('active');
-        }
+$(document).ready(function(){
+    $(window).scroll(function() { // check if scroll event happened
+      if ($(document).scrollTop() > 800) { // check if user scrolled more than 50 from top of the browser window
+        $(".fixed-top").css("background-color", "black"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+      } else {
+        $(".fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+      }
     });
-});
+  });
