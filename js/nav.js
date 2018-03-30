359caf901e549ad7224b14c7e8d7d0ec43a764db
Josue Del Rio <sandra@MacBook-Pro-de-Sandra-2.local>
2018-03-30 17:28:01 -0600

    commited fix

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(window).scroll(function() { // check if scroll event happened
-      if ($(document).scrollTop() > 800) { // check if user scrolled more than 50 from top of the browser window
-        $(".fixed-top").css("background-color", "black"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+      if ($(document).scrollTop() > 800) { // check if user scrolled more than 800 from top of the browser window
+        $(".fixed-top").css("background-color", "black"); // if yes, then change the color of class "fixed-top" to black 
       } else {
         $(".fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
       }
